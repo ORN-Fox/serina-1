b60959350e54e917a08358d2ef5e743832f583cb
Florian Armand <florianarmand05000@gmail.com>
2020-06-10 00:04:59 -0400

    feat: refactor isSecondInstance management because makeSingleInstance is remove to electron

--- app/main.js
+++ app/main.js
@@ -41,19 +41,10 @@ function createWindow () {
   })
 }
 
-const isSecondInstance = app.makeSingleInstance((commandLine, workingDirectory) => {
-  // Someone tried to run a second instance, we should focus our window.
-  if (mainWindow) {
-    if (mainWindow.isMinimized()) {
-      mainWindow.restore()
-    }
-    mainWindow.focus()
-  }
-})
-
-if (isSecondInstance) {
+app.requestSingleInstanceLock()
+app.on('second-instance', (event, argv, cwd) => {
   app.quit()
-}
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
