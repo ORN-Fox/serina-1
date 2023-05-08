075412491f88af2ca980772575c80f3113ce2ca9
Florian Armand <49326517+ORN-Fox@users.noreply.github.com>
2023-05-07 20:07:20 -0400

    feat: added base components for all app pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LanguagesComponent } from './pages/languages/languages.component';
+import { LevelComponent } from './pages/level/level.component';
+import { PreviewComponent } from './pages/preview/preview.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LanguagesComponent,
+    LevelComponent,
+    PreviewComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
