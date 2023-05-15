d3160d88c6158164e8f800279167fe9c1d4640d4
Florian Armand <49326517+ORN-Fox@users.noreply.github.com>
2023-05-15 09:56:20 -0400

    feat: added routes for each pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LanguagesComponent } from './pages/languages/languages.component';
+import { LevelComponent } from './pages/level/level.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { PreviewComponent } from './pages/preview/preview.component';
+
+const routes: Routes = [
+  { path: '/languages', component: LanguagesComponent },
+  { path: '/level', component: LevelComponent },
+  { path: '/preview', component: PreviewComponent },
+  { path: '/settings', component: SettingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
