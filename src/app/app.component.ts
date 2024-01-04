cc3a44e4b8b9ec30a5939eec064bd527ce823c34
Florian Armand <49326517+ORN-Fox@users.noreply.github.com>
2024-01-03 22:26:11 -0500

    feat: added themes system on app core

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { SettingsService } from './core/services/settings/settings.service';
+
+import { Settings } from './core/models/settings/settings.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'serina';
+  
+  settings: Settings;
+
+  constructor(private settingsService: SettingsService) {
+    this.settingsService.initSettings();
+    this.settings = this.settingsService.getSettings();
+
+    let linkTheme = document.createElement("link");
+    linkTheme.href = `/assets/styles/vendor/angular-material-prebuilt-themes/${this.settings.theme}.css`;
+    linkTheme.type = "text/css";
+    linkTheme.rel = "stylesheet";
+    linkTheme.media = "screen,print";
+    document.head.appendChild(linkTheme);
+  }
 }
