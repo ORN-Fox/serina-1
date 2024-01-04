9f2ea8347fc7a0fd45acb4856898eef323072dee
Florian Armand <49326517+ORN-Fox@users.noreply.github.com>
2024-01-03 22:24:55 -0500

    feat: added all imports need for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule,  } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule} from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// Commons Components
+import { ConfirmDialogComponent } from './core/components/confirm-dialog/confirm-dialog.component';
+import { BtnDownloadLanguageComponent } from './core/components/btn-download-language/btn-download-language.component';
+import { MenuToolbarComponent } from './core/components/menu-toolbar/menu-toolbar.component';
+import { MessageNoLanguageComponent } from './core/components/message-no-language/message-no-language.component';
+
+// Pages
 import { LanguagesComponent } from './pages/languages/languages.component';
 import { LevelComponent } from './pages/level/level.component';
 import { PreviewComponent } from './pages/preview/preview.component';
@@ -18,14 +39,35 @@ export function HttpLoaderFactory(http: HttpClient) {
 @NgModule({
   declarations: [
     AppComponent,
+
+    // Commons Components
+    ConfirmDialogComponent,
+    BtnDownloadLanguageComponent,
+    MessageNoLanguageComponent,
+
+    // Pages
     LanguagesComponent,
     LevelComponent,
     PreviewComponent,
-    SettingsComponent
+    SettingsComponent,
+    MenuToolbarComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    ScrollingModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
