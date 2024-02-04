a0ceafc4c7bb065b597b4488b27d1564d79b1d38
Florian Armand <49326517+ORN-Fox@users.noreply.github.com>
2024-02-04 11:48:00 -0500

    feat: added translation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,6 +35,7 @@ import { PreviewComponent } from './pages/preview/preview.component';
 import { SettingsComponent } from './pages/settings/settings.component';
 import { SidenavMenuComponent } from './core/components/sidenav-menu/sidenav-menu.component';
 import { TransaltionsLevelComponent } from './pages/translations-level/translations-level.component';
+import { TranslationComponent } from './core/components/translation/translation.component';
 import { TranslationsComponent } from './core/components/translations/translations.component';
 
 export function HttpLoaderFactory(http: HttpClient) {
@@ -57,6 +58,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     SettingsComponent,
     SidenavMenuComponent,
     TransaltionsLevelComponent,
+    TranslationComponent,
     TranslationsComponent,
   ],
   imports: [
---
+++ src/app/core/components/translation/translation.component.ts
@@ -0,0 +1,43 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { Translation } from '../../models/translation/translation.model';
+
+@Component({
+  selector: 'app-translation',
+  templateUrl: './translation.component.html',
+  styleUrls: ['./translation.component.scss']
+})
+export class TranslationComponent {
+
+  @Input() languages: string[];
+  @Input() translation: Translation;
+
+  @Output() TranslationComponentDidDeleteTranslationEvent: EventEmitter<{ translation: Translation }> = new EventEmitter();
+  @Output() TranslationComponentDidDuplicateTranslationEvent: EventEmitter<{ translation: Translation }> = new EventEmitter();
+  @Output() TranslationComponentDidGeneratePluralTranslationEvent: EventEmitter<{ translation: Translation }> = new EventEmitter();
+  @Output() TranslationComponentDidMoveTranslationEvent: EventEmitter<{ translation: Translation }> = new EventEmitter();
+  @Output() TranslationComponentDidSaveTranslationEvent: EventEmitter<{ translation: Translation }> = new EventEmitter();
+
+  constructor() {}
+
+  deleteTranslation(translation: Translation) {
+    this.TranslationComponentDidDeleteTranslationEvent.emit({ translation: translation });
+  }
+
+  duplicateTranslation(translation: Translation) {
+    this.TranslationComponentDidDuplicateTranslationEvent.emit({ translation: translation });
+  }
+
+  generatePluralTranslation(translation: Translation) {
+    this.TranslationComponentDidGeneratePluralTranslationEvent.emit({ translation: translation });
+  }
+
+  moveTranslation(translation: Translation) {
+    this.TranslationComponentDidMoveTranslationEvent.emit({ translation: translation });
+  }
+
+  saveTranslation(translation: Translation) {
+    this.TranslationComponentDidSaveTranslationEvent.emit({ translation: translation });
+  }
+
+}
